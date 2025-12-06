69bf08e35c014a358802863a93ad194754ceeb96
alaminsaleh <abusalehalamin52@gmail.com>
2025-12-06 13:33:17 +0600

    Loading for whole project

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
+
 const Loading = () => {
     return (
-        <div>
-
+        <div className="min-h-screen flex flex-col justify-center items-center bg-white">
+            <div className="w-10 h-10 bg-[#97a709] rounded-full animate-bounce"></div>
+            <p className="mt-4 text-gray-700 font-medium">Loading...</p>
         </div>
     );
 };
+
 
 export default Loading;

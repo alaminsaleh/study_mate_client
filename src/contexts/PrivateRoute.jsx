0595c183bcc57790eada9e107ade020d2542ba81
alaminsaleh <abusalehalamin52@gmail.com>
2025-12-06 13:44:53 +0600

    Implement private route functionalities

--- src/contexts/PrivateRoute.jsx
+++ src/contexts/PrivateRoute.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { use } from 'react';
+import { AuthContext } from './AuthContext';
+import Loading from '../components/Loading/Loading';
+import { Navigate } from 'react-router';
 
-const PrivateRoute = () => {
-    return (
-        <div>
 
-        </div>
-    );
+const PrivateRoute = ({ children }) => {
+
+
+    const { user, loading } = use(AuthContext);
+    if (loading) {
+        return <Loading></Loading>
+    }
+
+
+    if (user && user?.email) {
+        return children;
+    }
+
+
+    return <Navigate to='/login'></Navigate>
 };
+
 
 export default PrivateRoute;

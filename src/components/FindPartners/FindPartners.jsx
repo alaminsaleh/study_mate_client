0b2ab396a50f3d887ac5cea1e3602480d54f90e0
alaminsaleh <abusalehalamin52@gmail.com>
2025-12-06 13:31:12 +0600

    Build Find Partners page where get all the partners as card style

--- src/components/FindPartners/FindPartners.jsx
+++ src/components/FindPartners/FindPartners.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { useLoaderData } from 'react-router';
+import PartnerCard from '../Partner/PartnerCard';
+
 
 const FindPartners = () => {
+    const partner = useLoaderData();
     return (
-        <div>
-
+        <div className='grid grid-cols-3 gap-5 mx-auto max-w-4/5'>
+            {
+                partner.map(partner => <PartnerCard key={partner._id} partner={partner}></PartnerCard>)
+            }
         </div>
     );
 };
+
 
 export default FindPartners;

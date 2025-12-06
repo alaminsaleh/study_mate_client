191e5b4d90eeccf5d3ba6d7e5424d7ff0228d444
alaminsaleh <abusalehalamin52@gmail.com>
2025-12-06 13:32:20 +0600

    Build the Top Styde Partners section

--- src/components/HighRatingPartners/TopStudyPartners.jsx
+++ src/components/HighRatingPartners/TopStudyPartners.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { use } from 'react';
+import PartnerCard from '../Partner/PartnerCard';
 
-const TopStudyPartners = () => {
+
+const TopStudyPartners = ({ topStudyPartnersPromise }) => {
+    const partners = use(topStudyPartnersPromise);
+    console.log(partners);
     return (
         <div>
-
+            <h2>Top Partners</h2>
+            <div className='grid grid-cols-3 gap-5 mx-auto max-w-4/5'>
+                {
+                    partners.map(partner => <PartnerCard key={partner._id} partner={partner}></PartnerCard>)
+                }
+            </div>
         </div>
     );
 };
+
 
 export default TopStudyPartners;
